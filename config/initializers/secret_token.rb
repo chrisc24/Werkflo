23f50674ef27b2c585b625b4466c89bd8c4e2a7b
Chris Callagan <christopherbcallagan@gmail.com>
2011-04-10 15:55:34 -0700

    rails 3 now

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Werkflo::Application.config.secret_token = '92e6e96e11ecebb1a207e89188077ea244d6857f3a6e04175c662beb980d10371917a0123b4b3f083ccc8e9f29bbe9a3975287f92776b25c4777b8f96bc672c4'
